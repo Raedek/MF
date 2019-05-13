988a63c8425171ff140f991586de3d806d218b3f
Raedek <50552810+Raedek@users.noreply.github.com>
2019-05-13 09:14:46 +0200

    Update Main.java

--- Automation/src/Framework/Main.java
+++ Automation/src/Framework/Main.java
@@ -52,7 +52,6 @@ public class Main {
 
 		gFunc.Sleep(1000);
 		Chrome.bookFlight("roundtrip", 2, "New York", "June", 7, "San Francisco", "July", 6, "Economy class", "Blue Skies Airlines");	// airlines: {No Preference | Blue Skies Airlines | Unified Airlines | Pangea Airlines}
-		gFunc.Report(gVars.testRun, gVars.STEP3, "Successfull", true);
 		gFunc.Sleep(1000);
 		
 		Chrome.close();
